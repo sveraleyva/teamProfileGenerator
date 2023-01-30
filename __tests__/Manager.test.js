c39b89129f79b5c5f70fb96ed9f66d1cb978e6ba
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-01-29 18:12:55 -0600

    new gitignore, programmed the tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+const { exportAllDeclaration } = require("@babel/types");
+
+test("office number can be entered with the constructor", () => {
+  const testValue = 123;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
+
+test("getOffice() should return office number", () => {
+  const testValue = 123;
+  const e = new Manager("Foo", testValue, "test@test.com", "Manager");
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
+
+test("getRole() should return Manager", () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
